98ecf4818c86f1a029ff797eda2618f479ead0d9
Iknowmyname <yashveen77@gmail.com>
2026-01-08 12:20:19 +0800

    fix: added new vercel domain in CORS

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -20,6 +20,7 @@ app.use(
   cors({
     origin: [
       "https://flood-monitoring-system-live.vercel.app",
+      "https://flood-monitoring-system-drab.vercel.app",
       "http://localhost:5173",
       "http://127.0.0.1:5173",
     ],
